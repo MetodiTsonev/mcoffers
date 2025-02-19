9a76feaf113a192310cd189c64beda3d4f7a4004
Vladimir <vladimir.grig02@gmail.com>
2025-02-20 00:25:51 +0200

    navMenu completed

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,49 +1,125 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, IconButton, Drawer, List, ListItem, ListItemText, ListItemIcon, Divider, Button } from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
-import './NavMenu.css';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import LunchDiningIcon from '@mui/icons-material/LunchDining';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+const pages = ['Home', 'Schedule', 'Shift Pool', 'Income'];
 
 export function NavMenu() {
-  const [open, setOpen] = useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const toggleDrawer = () => {
-    setOpen(!open);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
-    <header>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h4">
-            <Link to="/" style={{ color: 'inherit', textDecoration: 'none', fontFamily: "cursive" }}>McOffers</Link>
+    <AppBar position="static" sx={{ bgcolor: 'navMenu.background' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <LunchDiningIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 5,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'cursive',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            McOffers
           </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+            anchor="left"
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            >
+            <List sx={{ width: 250 }}>
+              {pages.map((page) => (
+                <ListItem button key={page} onClick={handleCloseNavMenu}>
+                  <ListItemText primary={page} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          </Box>
+          <LunchDiningIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 5,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'cursive',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            McOffers
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> {/* TODO Add user image/ initials for the name of the user */}
+              </IconButton>
+          </Box>
         </Toolbar>
-      </AppBar>
-      <Drawer anchor="left" open={open} onClose={toggleDrawer}>
-        <div role="presentation" onClick={toggleDrawer} onKeyDown={toggleDrawer}>
-          <List>
-            <ListItem button component={Link} to="/" >
-              <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem button component={Link} to="/schedule">
-              <ListItemText primary="Schedule" />
-            </ListItem>
-            <ListItem button component={Link} to="/shiftPool">
-              <ListItemText primary="ShiftPool" />
-            </ListItem>
-            <ListItem button component={Link} to="/income">
-              <ListItemText primary="Income" />
-            </ListItem>
-            <ListItem button component={Link} to="/profile">
-              <ListItemText primary="Profile" />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
-    </header>
+      </Container>
+    </AppBar>
   );
-};
+}
